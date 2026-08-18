0720028f89e7494ca3b4c1b54fa0dae206ea71c4
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-08-18 20:50:41 +0530

    feat: add Hero section to landing page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <h1>Hero</h1>
-  )
-}
+    <div className="container p-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="TradeSphere investment platform"
+          className="img-fluid mb-5"
+        />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more.
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Sign up for free
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
