4340ea1a2e7919ecbd044c36b04b9d8f0aeb70aa
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-01 23:43:59 +0530

    Add navbar with React Router links

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import HomePage from "./landing_page/home/HomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>
+  </React.StrictMode>
 );
