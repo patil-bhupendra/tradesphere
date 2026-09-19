043eb343ae5e38133dde3a8064f646ac63684928
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-19 10:13:54 +0530

    feat: build TradeSphere pricing page

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import Brokerage from "./Brokerage";
+import OpenAccount from "../OpenAccount";
 
-const PricingPage = () => {
+function PricingPage() {
   return (
-    <h1>PricingPage</h1>
-  )
+    <>
+      <Hero />
+      <OpenAccount />
+      <Brokerage />
+    </>
+  );
 }
 
-export default PricingPage
+export default PricingPage;
