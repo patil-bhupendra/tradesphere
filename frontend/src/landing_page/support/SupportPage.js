46854004110ae49dda6b6b463a59cea9bd392c08
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-20 23:02:09 +0530

    feat: build TradeSphere support page

--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
 
-const SupportPage = () => {
+function SupportPage() {
   return (
-    <h1>SupportPage</h1>
-  )
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
-export default SupportPage
+export default SupportPage;
