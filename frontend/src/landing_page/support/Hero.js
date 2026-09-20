bd4c08f1ccb87185af2587b4e43d856745a527ec
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-09-20 22:52:05 +0530

    feat: add TradeSphere support hero

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
 
-const Hero = () => {
+function Hero() {
   return (
-    <h1>Hero</h1>
-  )
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5" id="supportWrapper">
+        <h4>TradeSphere Support</h4>
+        <a href="">My Tickets</a>
+      </div>
+
+      <div className="row p-5 m-3">
+        <div className="col-6 p-3">
+          <h1 className="fs-3" style={{ lineHeight: "1.5" }}>
+            Search for help or browse topics to find solutions
+          </h1>
+
+          <input
+            type="text"
+            placeholder="Eg. How do I place an order?"
+            className="mb-3 p-2"
+          />
+
+          <br />
+
+          <a href="">Account & Login</a>&nbsp;&nbsp;&nbsp;
+          <a href="">Orders & Trading</a>&nbsp;&nbsp;&nbsp;
+          <a href="">Portfolio</a>&nbsp;&nbsp;&nbsp;
+
+          <br />
+
+          <a href="">Technical Support</a>
+        </div>
+
+        <div className="col-6 p-3">
+          <h1 className="fs-3">Featured</h1>
+
+          <ol>
+            <li>
+              <a href="">Getting started with TradeSphere</a>
+            </li>
+
+            <br />
+
+            <li>
+              <a href="">Understanding orders and positions</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
